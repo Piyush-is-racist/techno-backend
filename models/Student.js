9da40c0fb64555a9dec2ed56e4de34d77a13ec47
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-20 13:37:55 +0530

    Update Student.js

--- models/Student.js
+++ models/Student.js
@@ -1,36 +1,61 @@
-// models/Student.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const StudentSchema = new mongoose.Schema({
-  tempRoll: String,
+const studentSchema = new mongoose.Schema({
   roll: String,
   dob: String,
   name: String,
+  phone: String,
+  email: String,
+  year: String,
+
   gender: String,
   caste: String,
   nationality: String,
   bloodGroup: String,
-  address: String,
+  address: { type: String, required: true },
   district: String,
   state: String,
   pin: String,
-  phone: String,
-  email: String,
+  
   fatherName: String,
   fatherOccupation: String,
   fatherDesignation: String,
   fatherAge: String,
   fatherMobile: String,
+  fatherPan: String,
+  fatherPassport: String,
+  fatherAddress: { type: String, required: true },
+  fatherPin: String,
+  fatherDistrict: String,
+
+  
   motherName: String,
   motherOccupation: String,
   motherDesignation: String,
   motherAge: String,
   motherMobile: String,
+  motherPan: String,
+  motherPassport: String,
+  motherAddress: { type: String, required: true },
+  motherPin: String,
+  motherDistrict: String,
+
   guardianName: String,
   enrollmentNo: String,
   stream: String,
   examName: String,
-  examRank: String
+  examRank: String,
+
+  studentPan: String,
+  studentPassport: String,
+
+  password: String,
+
+  marks: Object,
+  homework: Array,
+  classwork: Array,
+  fees: Object,
+  attendance: Object
 });
 
-module.exports = mongoose.model('Student', StudentSchema);
+module.exports = mongoose.model("Student", studentSchema);
