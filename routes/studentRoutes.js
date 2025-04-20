7e03fdffd9df7a5bee4ce17db27111cba6bb1c0e
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-20 15:04:21 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Student = require("../models/studentModel");
+const Student = require("../models/Student");
 
 // LOGIN route
 router.post("/login", async (req, res) => {
