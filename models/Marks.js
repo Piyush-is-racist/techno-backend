35c470925092d731af9a7238a696d16db67e71a3
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-21 15:33:34 +0530

    Update Marks.js

--- models/Marks.js
+++ models/Marks.js
@@ -1,18 +1,33 @@
 const mongoose = require("mongoose");
 
+const caSchema = new mongoose.Schema({
+  ca1: { type: Number, default: 0 },
+  ca2: { type: Number, default: 0 },
+  ca3: { type: Number, default: 0 },
+  ca4: { type: Number, default: 0 }
+}, { _id: false });
+
+const subjectSchema = new mongoose.Schema({
+  sub1: { type: caSchema, default: () => ({}) },
+  sub2: { type: caSchema, default: () => ({}) },
+  sub3: { type: caSchema, default: () => ({}) },
+  sub4: { type: caSchema, default: () => ({}) },
+  sub5: { type: caSchema, default: () => ({}) }
+}, { _id: false });
+
 const marksSchema = new mongoose.Schema({
   roll: String,
   name: String,
   year: String,
   marks: {
-    sem1: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem2: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem3: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem4: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem5: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem6: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem7: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
-    sem8: { ca1: Number, ca2: Number, ca3: Number, ca4: Number },
+    sem1: { type: subjectSchema, default: () => ({}) },
+    sem2: { type: subjectSchema, default: () => ({}) },
+    sem3: { type: subjectSchema, default: () => ({}) },
+    sem4: { type: subjectSchema, default: () => ({}) },
+    sem5: { type: subjectSchema, default: () => ({}) },
+    sem6: { type: subjectSchema, default: () => ({}) },
+    sem7: { type: subjectSchema, default: () => ({}) },
+    sem8: { type: subjectSchema, default: () => ({}) }
   }
 });
 
