bbd9edfd5ca361536a8323daa646e7082e12d218
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-23 05:03:27 +0530

    Create Classwork.js

---
+++ models/Classwork.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const classworkSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  fileURL: String,
+  date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Classwork", classworkSchema);
