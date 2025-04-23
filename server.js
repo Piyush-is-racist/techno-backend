55ba30d3c0aa8f45057d409a6dc03fc357428eed
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-23 06:53:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
   const PORT = process.env.PORT || 10000;
   app.listen(PORT, () => {
-    console.log(🚀 Server running on port ${PORT});
+    console.log(`🚀 Server running on port ${PORT}`);
   });
 })
 .catch((err) => {
