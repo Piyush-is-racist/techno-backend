cc7345aa1b3dbc27b6faa06fa51b1fc8bbf06681
Piyush Singh <149002604+Piyush-is-racist@users.noreply.github.com>
2025-04-23 20:58:04 +0530

    Update Classwork.js

--- models/Classwork.js
+++ models/Classwork.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const classworkSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: String,
-  fileURL: String,
+  description: { type: String, required: true },
+  fileURL: { type: String },
+  yearGroup: { type: String, default: "All" }, // "1", "2", "3", "4", or "All"
   date: { type: Date, default: Date.now }
 });
 
